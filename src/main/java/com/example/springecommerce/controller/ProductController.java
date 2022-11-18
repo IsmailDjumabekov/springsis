3244ff5124540ed4204d2288c3daddab50b5aaa2
ismail <ismaildjumabekov@gmail.com>
2022-11-18 18:47:11 +0600

    Functional completed crud

--- src/main/java/com/example/springecommerce/controller/ProductController.java
+++ src/main/java/com/example/springecommerce/controller/ProductController.java
@@ -1,15 +1,38 @@
 package com.example.springecommerce.controller;
 
 
+import com.example.springecommerce.model.Product;
+import com.example.springecommerce.model.User;
+import com.example.springecommerce.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("")
     public String show(){
         return "products/show";
+    }
+    @GetMapping("/create")
+    public String create(){
+        return "products/create";
+    }
+    @PostMapping("/save")
+    public String save(Product product){
+        LOGGER.info("Product save successfuly {}", product);
+        User user = new User("","","","","","","",1);
+        product.setUser(user);
+        productService.save(product);
+        return "redirect:/products";
     }
 }
