e3568acfca2fd610cbacd2643fd96259b19ed192
ismail <ismaildjumabekov@gmail.com>
2022-11-19 21:04:24 +0600

    upliad image completed

--- src/main/java/com/example/springecommerce/controller/ProductController.java
+++ src/main/java/com/example/springecommerce/controller/ProductController.java
@@ -3,17 +3,16 @@ package com.example.springecommerce.controller;
 import com.example.springecommerce.model.Product;
 import com.example.springecommerce.model.User;
 import com.example.springecommerce.service.ProductService;
-import org.jetbrains.annotations.NotNull;
+import com.example.springecommerce.service.UploadFileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -22,6 +21,9 @@ public class ProductController {
     private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private UploadFileService uploadFileService;
 
     @GetMapping("")
     public String show(Model model){
@@ -33,9 +35,22 @@ public class ProductController {
         return "products/create";
     }
     @PostMapping("/save")
-    public String save(Product product){
+    public String save(Product product, @RequestParam("img") MultipartFile file) throws IOException {
         LOGGER.info("Product save successfuly {}", product);
         User user = new User(1,"","","","","","","");
+        if(product.getId()==null){
+            String nameImage = uploadFileService.saveImage(file);
+            product.setImage(nameImage);
+        }else{
+            if (file.isEmpty()){
+                Product product1 = new Product();
+                product1 = productService.get(product.getId()).get();
+                product.setImage(product1.getImage());
+            }else{
+                String nameImage = uploadFileService.saveImage(file);
+                product.setImage(nameImage);
+            }
+        }
         product.setUser(user);
         productService.save(product);
         return "redirect:/products";
