aa0814a2e9597438f0ceefbd0793cbceb3f14a6f
ismail <ismaildjumabekov@gmail.com>
2022-11-20 13:33:06 +0600

    counting the entire basket

--- src/main/java/com/example/springecommerce/controller/HomeController.java
+++ src/main/java/com/example/springecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.springecommerce.controller;
 
+import com.example.springecommerce.model.DetailOrder;
+import com.example.springecommerce.model.Order;
 import com.example.springecommerce.model.Product;
 import com.example.springecommerce.service.ProductService;
 import org.slf4j.Logger;
@@ -7,10 +9,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -20,6 +22,10 @@ public class HomeController {
     private final Logger log = LoggerFactory.getLogger(HomeController.class);
     @Autowired
     private ProductService productService;
+
+    List<DetailOrder> detail = new ArrayList<DetailOrder>();
+
+    Order order = new Order();
 
     @GetMapping("")
     public String home(Model model) {
@@ -34,5 +40,30 @@ public class HomeController {
         product = productOptional.get();
         model.addAttribute("product", product);
         return "user/producthome";
+    }
+    @PostMapping("/cart")
+    public String addCart(@RequestParam Integer id, @RequestParam Integer quantity,Model model){
+        DetailOrder detailOrder = new DetailOrder();
+        Product product = new Product();
+        double sumTotal = 0;
+        Optional<Product> optionalProduct = productService.get(id);
+        log.info("Product added: {}", optionalProduct.get());
+        log.info("Quantity: {}", quantity);
+        product = optionalProduct.get();
+        detailOrder.setQuantity(quantity);
+        detailOrder.setPrice(product.getPrice());
+        detailOrder.setName(product.getName());
+        detailOrder.setTotal(product.getPrice() * quantity);
+        detailOrder.setProduct(product);
+
+        detail.add(detailOrder);
+
+        sumTotal = detail.stream().mapToDouble(dt -> dt.getTotal()).sum();
+
+        order.setTotal(sumTotal);
+        model.addAttribute("cart", detail);
+        model.addAttribute("order", order);
+
+        return "user/cart";
     }
 }
